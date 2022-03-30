206ef40a16714f0a2ad64dce338040fe2a09b549
Charles <tietjen.charles@gmail.com>
2022-03-30 00:24:04 -0400

    minor tweaks

--- chat-client/src/controllers/socket.js
+++ chat-client/src/controllers/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:8080/');
+export const socket = io('https://not-discord-chat-server.herokuapp.com/');
 
 socket.on('connect', () => socket.emit('hello', `${socket.id} has arrived!`));
 socket.on('welcome', msg => {console.log(msg)})
